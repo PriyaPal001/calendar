9524419a7d8618f3c12e561a756c2540852ee4ba
PriyaPal001 <vu3t1s2021032@pvppcoe.ac.in>
2023-05-28 09:49:23 +0530

    added structure

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import DatePicker from './lib/DataPicker.svelte'
+	let cmpDatePicker
+	
+	function datepicked (e) {
+		console.log(e.detail.datepicked)
+	}
+</script>
+
+<DatePicker bind:this={cmpDatePicker} on:datepicked={datepicked} />
+
+<svelte:head>
+	<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+</svelte:head>
